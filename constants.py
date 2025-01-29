d40e75de822652edd3867726db7c29780e350589
Andrey66 <andrey66-66-66@yandex.ru>
2025-01-29 08:57:43 +0300

    Linter fix

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 GOOGLE_SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets.readonly',
-    'https://www.googleapis.com/auth/drive.readonly',
+    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://www.googleapis.com/auth/drive.readonly",
 ]
 
-GOOGLE_CREDENTIALS_FILE = 'yabar-bot-2edc386bc135.json'
-SPREADSHEET_ID = '1h5w87qaNnXD0RO5xHJzEwPzoCuwH-D_SBPQN-6Jz42Q'
+GOOGLE_CREDENTIALS_FILE = "yabar-bot-2edc386bc135.json"
+SPREADSHEET_ID = "1h5w87qaNnXD0RO5xHJzEwPzoCuwH-D_SBPQN-6Jz42Q"
 SPREADSHEET_RANGE_RECIPES = "'Меню'!A2:C"
 SPREADSHEET_RANGE_INGREDIENTS = "'Ингредиенты'!A2:D"
 SPREADSHEET_RANGE_COCKTAILS = "'Настройки'!B2:B"
 SPREADSHEET_RANGE_AVAILABLE_COCKTAILS = "'Меню'!A2:A"
-MENU_TITLE = '📖 МЕНЮ:\n'
-COCKTAIL_SMILE = '🍹'
+MENU_TITLE = "📖 МЕНЮ:\n"
+COCKTAIL_SMILE = "🍹"
--- utils.py
+++ utils.py
@@ -1,34 +1,35 @@
 from typing import Dict, List
 
-from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-
 from constants import COCKTAIL_SMILE
+from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def str_to_markdown(string: str) -> str:
-    string = string.replace('%', '\\%')
-    string = string.replace('#', '\\#')
-    string = string.replace('+', '\\+')
-    string = string.replace('&', '\\&')
-    string = string.replace('-', '\\-')
-    string = string.replace('.', '\\.')
+    string = string.replace("%", r"\%")
+    string = string.replace("#", r"\#")
+    string = string.replace("+", r"\+")
+    string = string.replace("&", r"\&")
+    string = string.replace("-", r"\-")
+    string = string.replace(".", r"\.")
     return string
 
 
-def menu_dict_to_str(menu: Dict[str, List[str]], menu_title: str = '') -> str:
-    answer = ''
-    if menu_title != '':
-        answer = '*' + menu_title + '*'
+def menu_dict_to_str(menu: Dict[str, List[str]], menu_title: str = "") -> str:
+    answer = ""
+    if menu_title != "":
+        answer = "*" + menu_title + "*"
     for cocktail in menu.keys():
-        answer += str_to_markdown(COCKTAIL_SMILE) + '*' + str_to_markdown(cocktail) + '*\n'
-        for ingredient in menu.get(cocktail):
-            answer += str_to_markdown('- ' + ingredient) + '\n'
+        answer += (
+            str_to_markdown(COCKTAIL_SMILE) + "*" + str_to_markdown(cocktail) + "*\n"
+        )
+        for ingredient in menu.get(cocktail, []):
+            answer += str_to_markdown("- " + ingredient) + "\n"
     return answer
 
 
 def range_to_dict(recipes_range: list, ingredients_range: list) -> Dict[str, List[str]]:
     ingredients = {}
-    menu = {}
+    menu: Dict[str, List[str]] = {}
     for ingredient in ingredients_range:
         ingredients[ingredient[0]] = ingredient[3]
 
@@ -36,7 +37,7 @@ def range_to_dict(recipes_range: list, ingredients_range: list) -> Dict[str, Lis
         cocktail = row[0]
         ingredient = row[1]
         if not menu.get(cocktail):
-            if ingredients.get(ingredient) == 'нет':
+            if ingredients.get(ingredient) == "нет":
                 menu[cocktail] = [ingredient]
         else:
             menu[cocktail].append(ingredient)
@@ -47,26 +48,38 @@ def build_menu_buttons(menu: Dict[str, List[str]]) -> InlineKeyboardMarkup:
     buttons = []
     cocktails = list(menu.keys())
     for i in range(int(len(cocktails) / 2)):
-        buttons.append([InlineKeyboardButton(cocktails[i * 2], callback_data=cocktails[i * 2]),
-                        InlineKeyboardButton(cocktails[i * 2 + 1], callback_data=cocktails[i * 2 + 1])])
+        buttons.append(
+            [
+                InlineKeyboardButton(cocktails[i * 2], callback_data=cocktails[i * 2]),
+                InlineKeyboardButton(
+                    cocktails[i * 2 + 1], callback_data=cocktails[i * 2 + 1]
+                ),
+            ]
+        )
+
     if len(cocktails) % 2 > 0:
-        buttons.append(([InlineKeyboardButton(cocktails[-1], callback_data=cocktails[-1])]))
-    buttons.append([InlineKeyboardButton('Показать списком', callback_data='show_menu_list'),
-                    InlineKeyboardButton('Обновить', callback_data='action_selection_menu')])
+        buttons.append(
+            ([InlineKeyboardButton(cocktails[-1], callback_data=cocktails[-1])])
+        )
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    "Показать списком", callback_data="show_menu_list"
+                ),
+                InlineKeyboardButton("Обновить", callback_data="action_selection_menu"),
+            ]
+        )
     return InlineKeyboardMarkup(buttons)
 
 
-async def update_message(query: CallbackQuery, buttons: InlineKeyboardMarkup, text: str, parse_mode=None) -> None:
+async def update_message(
+    query: CallbackQuery, buttons: InlineKeyboardMarkup, text: str, parse_mode=None
+) -> None:
     if query.message.reply_markup == buttons and query.message.text == text:
         return
     if parse_mode:
         await query.edit_message_text(
-            text=text,
-            reply_markup=buttons,
-            parse_mode=parse_mode
+            text=text, reply_markup=buttons, parse_mode=parse_mode
         )
     else:
-        await query.edit_message_text(
-            text=text,
-            reply_markup=buttons
-        )
+        await query.edit_message_text(text=text, reply_markup=buttons)
