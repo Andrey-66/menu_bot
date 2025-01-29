d1d26ced570fe226a6fe86cc8deac95090d4f946
Andrey66 <andrey66-66-66@yandex.ru>
2025-01-29 08:57:43 +0300

    Linter fix

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 from datetime import datetime
+from typing import Any, Callable, Coroutine, Dict, Optional
 
+from constants import (
+    MENU_TITLE,
+    SPREADSHEET_ID,
+    SPREADSHEET_RANGE_AVAILABLE_COCKTAILS,
+    SPREADSHEET_RANGE_COCKTAILS,
+    SPREADSHEET_RANGE_INGREDIENTS,
+    SPREADSHEET_RANGE_RECIPES,
+)
 from dotenv import load_dotenv
-from telegram import (CallbackQuery, InlineKeyboardButton,
-                      InlineKeyboardMarkup, Update)
-from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
-                          ContextTypes)
-
-from constants import (MENU_TITLE, SPREADSHEET_ID,
-                       SPREADSHEET_RANGE_AVAILABLE_COCKTAILS,
-                       SPREADSHEET_RANGE_COCKTAILS,
-                       SPREADSHEET_RANGE_INGREDIENTS,
-                       SPREADSHEET_RANGE_RECIPES)
 from spreadsheets import google_auth, read_menu, read_sheet
-from utils import (build_menu_buttons, menu_dict_to_str, str_to_markdown,
-                   update_message)
+from telegram import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+from utils import build_menu_buttons, menu_dict_to_str, str_to_markdown, update_message
 
 load_dotenv()
 
@@ -23,87 +23,121 @@ SERVICE, CREDENTIALS = google_auth()
 
 async def wake_up(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
-    buttons = InlineKeyboardMarkup([
+    buttons = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton('Показать меню', callback_data='action_selection_menu'),
-        ],
-    ])
+            [
+                InlineKeyboardButton(
+                    "Показать меню", callback_data="action_selection_menu"
+                ),
+            ],
+        ]
+    )
     await context.bot.send_message(
-        chat_id=chat.id,
-        text='Спасибо, что включили меня',
-        reply_markup=buttons
+        chat_id=chat.id, text="Спасибо, что включили меня", reply_markup=buttons
     )
 
 
 async def error(query: CallbackQuery):
-    buttons = InlineKeyboardMarkup([
+    buttons = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton('Показать меню', callback_data='action_selection_menu'),
-        ],
-    ])
-    await update_message(query, buttons, 'Что-то пошло не так, начнём с начала?')
+            [
+                InlineKeyboardButton(
+                    "Показать меню", callback_data="action_selection_menu"
+                ),
+            ],
+        ]
+    )
+    await update_message(query, buttons, "Что-то пошло не так, начнём с начала?")
 
 
-async def show_menu_list(query: CallbackQuery) -> None:
-    buttons = InlineKeyboardMarkup([
+async def show_menu_list(
+    query: CallbackQuery, text: Optional[str] = MENU_TITLE
+) -> None:
+    if text is None:
+        text = ""
+    buttons = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton("Назад", callback_data="action_selection_menu"),
-            InlineKeyboardButton("Обновить", callback_data="update_list"),
-        ],
-    ])
+            [
+                InlineKeyboardButton("Назад", callback_data="action_selection_menu"),
+                InlineKeyboardButton("Обновить", callback_data="update_list"),
+            ],
+        ]
+    )
     await update_message(
         query,
         buttons,
-        menu_dict_to_str(read_menu(SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_RECIPES,
-                                   SPREADSHEET_RANGE_INGREDIENTS), menu_title=MENU_TITLE) + str_to_markdown(
-            f'\n\n UPD: {datetime.now()}'),
-        parse_mode='MarkdownV2'
+        menu_dict_to_str(
+            read_menu(
+                SERVICE,
+                SPREADSHEET_ID,
+                SPREADSHEET_RANGE_RECIPES,
+                SPREADSHEET_RANGE_INGREDIENTS,
+            ),
+            menu_title=text,
+        )
+        + str_to_markdown(f"\n\n UPD: {datetime.now()}"),
+        parse_mode="MarkdownV2",
     )
 
 
-async def show_menu(query: CallbackQuery, text='Нажми на коктейль, чтобы узнать подробнее') -> None:
-    print(text)
-    menu = read_menu(SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_RECIPES, SPREADSHEET_RANGE_INGREDIENTS)
+async def show_menu(
+    query: CallbackQuery,
+    text: Optional[str] = "Нажми на коктейль, чтобы узнать подробнее",
+) -> None:
+    menu = read_menu(
+        SERVICE,
+        SPREADSHEET_ID,
+        SPREADSHEET_RANGE_RECIPES,
+        SPREADSHEET_RANGE_INGREDIENTS,
+    )
     buttons = build_menu_buttons(menu)
+    if text is None:
+        text = ""
     await update_message(query, buttons, text)
 
 
 async def show_cocktail(query: CallbackQuery) -> None:
-    menu = read_menu(SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_RECIPES, SPREADSHEET_RANGE_INGREDIENTS)
-    buttons = InlineKeyboardMarkup([
+    menu = read_menu(
+        SERVICE,
+        SPREADSHEET_ID,
+        SPREADSHEET_RANGE_RECIPES,
+        SPREADSHEET_RANGE_INGREDIENTS,
+    )
+    buttons = InlineKeyboardMarkup(
         [
-            InlineKeyboardButton("Назад", callback_data="action_selection_menu")
-        ],
-    ])
-    cocktail = {
-        query.data: menu.get(query.data)
-    }
+            [InlineKeyboardButton("Назад", callback_data="action_selection_menu")],
+        ]
+    )
+    cocktail = {query.data: menu.get(query.data)}
     text = menu_dict_to_str(cocktail)
     if query.message.reply_markup != buttons:
         await query.edit_message_text(
-            text=text,
-            reply_markup=buttons,
-            parse_mode='MarkdownV2'
+            text=text, reply_markup=buttons, parse_mode="MarkdownV2"
         )
 
 
 async def buttons_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    FUNCTIONS = {
-        'update_list': show_menu_list,
-        'show_menu_list': show_menu_list,
-        'action_selection_menu': show_menu
+    FUNCTIONS: Dict[
+        str, Callable[[CallbackQuery, Optional[str]], Coroutine[Any, Any, None]]
+    ] = {
+        "update_list": show_menu_list,
+        "show_menu_list": show_menu_list,
+        "action_selection_menu": show_menu,
     }
     chat = update.effective_chat
-    await context.bot.send_chat_action(chat_id=chat.id, action='typing')
+    await context.bot.send_chat_action(chat_id=chat.id, action="typing")
     query = update.callback_query
     await query.answer()
     print(query.data)
     if query.data in FUNCTIONS.keys():
-        await FUNCTIONS[query.data](query)
+        FUNCTIONS[query.data](query)  # type: ignore
     else:
-        cocktails_range = read_sheet(SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_COCKTAILS).get('values')
-        available_cocktails_range = read_sheet(SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_AVAILABLE_COCKTAILS).get(
-            'values')
+        cocktails_range = read_sheet(
+            SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_COCKTAILS
+        ).get("values")
+        available_cocktails_range = read_sheet(
+            SERVICE, SPREADSHEET_ID, SPREADSHEET_RANGE_AVAILABLE_COCKTAILS
+        ).get("values")
         cocktails = set()
         available_cocktails = set()
         for row in available_cocktails_range:
@@ -115,16 +149,16 @@ async def buttons_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         if query.data in available_cocktails:
             await show_cocktail(query)
         elif query.data in cocktails:
-            await show_menu(query, 'Коктейль закончился :(')
+            await show_menu(query, "Коктейль закончился :(")
         else:
             await error(query)
 
 
 def main() -> None:
-    auth_token = os.getenv('TOKEN')
+    auth_token = os.getenv("TOKEN")
     application = Application.builder().token(auth_token).build()
     application.add_handler(CallbackQueryHandler(buttons_handler))
-    application.add_handler(CommandHandler('start', wake_up))
+    application.add_handler(CommandHandler("start", wake_up))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
--- spreadsheets.py
+++ spreadsheets.py
@@ -1,21 +1,21 @@
+from constants import GOOGLE_CREDENTIALS_FILE, GOOGLE_SCOPES
 from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
-
-from constants import GOOGLE_CREDENTIALS_FILE, GOOGLE_SCOPES
 from utils import range_to_dict
 
 
 def google_auth():
     credentials = Credentials.from_service_account_file(
-        filename=GOOGLE_CREDENTIALS_FILE, scopes=GOOGLE_SCOPES)
-    service = discovery.build('sheets', 'v4', credentials=credentials)
+        filename=GOOGLE_CREDENTIALS_FILE, scopes=GOOGLE_SCOPES
+    )
+    service = discovery.build("sheets", "v4", credentials=credentials)
     return service, credentials
 
 
 def read_menu(service, spreadsheet_id, recipe_range, ingredients_range):
     recipes = read_sheet(service, spreadsheet_id, recipe_range)
     ingredients = read_sheet(service, spreadsheet_id, ingredients_range)
-    return range_to_dict(recipes.get('values'), ingredients.get('values'))
+    return range_to_dict(recipes.get("values"), ingredients.get("values"))
 
 
 def read_cocktails_id(service, spreadsheet_id, cocktails_range):
@@ -27,9 +27,13 @@ def read_cocktails_id(service, spreadsheet_id, cocktails_range):
 
 
 def read_sheet(service, spreadsheet_id, sheet_range):
-    request = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id,
-        range=sheet_range,
+    request = (
+        service.spreadsheets()
+        .values()
+        .get(
+            spreadsheetId=spreadsheet_id,
+            range=sheet_range,
+        )
     )
     result = request.execute()
     return result
